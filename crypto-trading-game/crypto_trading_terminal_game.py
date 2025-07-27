60fb42da93e0091173a6e5ac2b2c0df8760a9dd6
Jonathan C. Lindo, Jr. <jonathanlindo.jr2008@gmail.com>
2025-07-27 08:53:59 +0800

    Update crypto_trading_terminal_game.py

--- crypto-trading-game/crypto_trading_terminal_game.py
+++ crypto-trading-game/crypto_trading_terminal_game.py
@@ -1,38 +1,50 @@
-import time
-import sys
-import intro_message_animation_crypto_trading_terminal_game
 import os
-# def welcomemessage(text, delay):
-#     for character in text:
-#         sys.stdout.write(character)
-#         sys.stdout.flush()
-#         time.sleep(delay)
-#     print()
-# message = "CRYPTO Tradeing"
-# welcomemessage(message, delay=0.04)
+import intro_message_animation_crypto_trading_terminal_game as intro_module
+import h_command
+import q_command
+import time
 
-# WELCOME MESSAGE HERE
+def intro():
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+    frames = [
+            intro_module.A,
+            intro_module.B,
+            intro_module.C,
+            intro_module.D,
+            intro_module.E,
+            intro_module.F,
+        ]
+    
+    delay = 0.09
+    intro_module.introanimate(frames, delay)
+    
 def commands():
-    import intro_message_animation_crypto_trading_terminal_game
+    while True:
+        intro()
+        user_command = input("""
 
-    user_command = input("""
-        
-        
     (q) Quick Start
-                  
-    (h) Help           
-            
+                             
+    (h) Help
+                             
     (e) Exit
-                    
-                                
-    Enter command:  """)
-    if user_command == 'q':
-        print('q command')
-    elif user_command == 'h':
-        import h_command
-    # elif user_command == 'e':
-        
 
-commands()
+    Enter command: """).strip().lower()
 
+        if user_command == 'q':
+            result = q_command.starting_message()
+
+        elif user_command == 'h':
+            result = h_command.show_help()  # show help and wait for user to go back
+            
+
+        elif user_command == 'e':
+            print("Exiting Coin$hell...")
+            break
+        else:
+            print("Invalid command. Try again.")
+
+if __name__ == '__main__':
+    intro()
+    commands()
